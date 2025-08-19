95babec0847d75b81a89277873b6fb5a6138b59f
Omar Abouzeid <omarabouzeid@Omars-MacBook-Pro.local>
2025-08-18 21:57:47 -0400

    chore: add cors to io and express server

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { createServer } from 'node:http';
 import express from 'express';
+import cors from 'cors';
 import { Server } from 'socket.io';
 import dotenv from 'dotenv';
 import { PrismaClient } from '../generated/prisma/index.js';
@@ -13,8 +14,18 @@ dotenv.config();
 export const prisma = new PrismaClient();
 const app = express();
 const server = createServer(app);
-export const io = new Server(server);
+export const io = new Server(server, {
+	cors: {
+		origin: '*',
+		methods: ['GET', 'POST'],
+	},
+});
 app.use(express.json());
+app.use(
+	cors({
+		origin: '*',
+	})
+);
 
 app.use('/users', userRouter);
 app.use('/rooms', roomRouter);
