054e79a94d2dcf2407b8c91879a464af40cecf9d
Omar Abouzeid <omarabouzeid@Omars-MacBook-Pro.local>
2025-08-18 22:53:35 -0400

    chore: add prisma helpers

--- src/prisma/helpers.ts
+++ src/prisma/helpers.ts
@@ -2,12 +2,20 @@ import { prisma } from '../index.js';
 import HttpError from '../classes/HttpError.js';
 import { Prisma } from '../../generated/prisma/index.js';
 
-export const findUser = async (userId: number) => {
+export const findUser = async <
+	T extends Prisma.RoomInclude | undefined = undefined
+>(
+	userId: number,
+	userInclude?: Prisma.UserInclude
+) => {
 	const user = await prisma.user.findFirst({
 		where: {
 			id: {
 				equals: userId,
 			},
+		},
+		include: {
+			...userInclude,
 		},
 	});
 
@@ -18,16 +26,20 @@ export const findUser = async (userId: number) => {
 	return user;
 };
 
-export const findRoom = async (roomId: string) => {
+export const findRoom = async <
+	T extends Prisma.RoomInclude | undefined = undefined
+>(
+	roomId: string,
+	prismaInclude?: Prisma.RoomInclude
+) => {
 	const room = await prisma.room.findFirst({
 		where: {
 			uuid: {
 				equals: roomId,
 			},
 		},
-		select: {
-			uuid: true,
-			users: true,
+		include: {
+			...prismaInclude,
 		},
 	});
 
