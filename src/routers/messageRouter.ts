d08f0ddaee2a6b2fb43d98fdfeb9791df474fe90
Omar Abouzeid <omarabouzeid@Omars-MacBook-Pro.local>
2025-08-30 23:06:46 -0400

    chore: add get room messages endpoint

--- src/routers/messageRouter.ts
+++ src/routers/messageRouter.ts
@@ -51,19 +51,64 @@ messageRouter.post('/send/:roomId', async (req, res) => {
 		omit: {
 			id: true,
 			roomId: true,
-			userId: true,
 			updatedAt: true,
 		},
 		include: {
-			user: {
-				select: {
-					name: true,
-				},
-			},
+			user: true,
 		},
 	});
 
 	io.to(room.uuid).emit('NEW_MESSAGE', createdMessage);
 
 	res.status(201).json(createdMessage);
+});
+
+messageRouter.get('/:roomId', async (req, res) => {
+	const { roomId } = req.params ?? {};
+	const { userId } = req.query ?? {};
+
+	if (!roomId || !userId) {
+		throw new HttpError('Missing roomId or userId.', 400);
+	}
+
+	const room = await prisma.room.findFirst({
+		where: {
+			uuid: {
+				equals: roomId,
+			},
+			users: {
+				some: {
+					id: Number(userId),
+				},
+			},
+		},
+	});
+
+	if (!room) {
+		throw new HttpError(
+			`User with ID: ${userId} is not part of this chat`,
+			400
+		);
+	}
+
+	const messages = await prisma.message.findMany({
+		where: {
+			room: {
+				uuid: {
+					equals: roomId,
+				},
+			},
+		},
+		omit: {
+			id: true,
+			updatedAt: true,
+			userId: true,
+			roomId: true,
+		},
+		include: {
+			user: true,
+		},
+	});
+
+	res.status(200).json(messages);
 });
